ca2d14892f576adbef2d2a1fbede095de03c8b46
James Morey <jamesrmorey@gmail.com>
2026-01-21 20:22:30 +0000

    chore: change user resource

--- app/Http/Resources/RouteResource.php
+++ app/Http/Resources/RouteResource.php
@@ -16,7 +16,6 @@ class RouteResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'user_id' => $this->user_id,
             'name' => $this->name,
             'slug' => $this->slug,
             'notes' => $this->notes,
