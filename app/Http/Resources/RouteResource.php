aed31971214ab21d0eb66b5f9868a9d05a791bb7
James Morey <jamesrmorey@gmail.com>
2026-01-22 19:51:38 +0000

    chore: remove id on server routes

--- app/Http/Resources/RouteResource.php
+++ app/Http/Resources/RouteResource.php
@@ -15,7 +15,6 @@ class RouteResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
             'server_id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
