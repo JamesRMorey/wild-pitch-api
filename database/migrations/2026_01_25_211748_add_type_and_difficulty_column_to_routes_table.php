6c24913222375c1d1edd1c52677f62c254f3eea2
James Morey <jamesrmorey@gmail.com>
2026-01-26 19:06:51 +0000

    chore: add default

--- database/migrations/2026_01_25_211748_add_type_and_difficulty_column_to_routes_table.php
+++ database/migrations/2026_01_25_211748_add_type_and_difficulty_column_to_routes_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('routes', function (Blueprint $table) {
-            $table->enum('type', ['CIRCULAR', 'POINT_TO_POINT', 'OUT_AND_BACK', 'UNKNOWN'])->nullable()->after('elevation_loss');
-            $table->enum('difficulty', ['EASY', 'MODERATE', 'CHALLENGING', 'DIFFICULT', 'UNKNOWN'])->nullable()->after('type');
+            $table->enum('type', ['CIRCULAR', 'POINT_TO_POINT', 'OUT_AND_BACK', 'UNKNOWN'])->default('UNKNOWN')->after('elevation_loss');
+            $table->enum('difficulty', ['EASY', 'MODERATE', 'CHALLENGING', 'DIFFICULT', 'UNKNOWN'])->default('UNKNOWN')->after('type');
         });
     }
 
